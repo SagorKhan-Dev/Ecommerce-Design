186b1a90617ebbc223afddc7c31a69290c06eb0f
SagorKhan-Dev <sagorkhan.cit@gmail.com>
2024-03-15 03:06:32 +0600

    setting up react router dom

---
+++ src/components/Layouts/RootLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Header from "../Header/Header";
+import { Outlet } from "react-router-dom";
+
+const RootLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Header />
+      <Outlet />
+    </>
+  );
+};
+
+export default RootLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from "./components/Layouts/RootLayout.jsx";
+import Home from "./components/pages/Home.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
